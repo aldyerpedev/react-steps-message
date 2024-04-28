456a1d69d53a6191c620cc43faf453edfb96507b
Aldy Rizky Putra <aldyerpedevs@gmail.com>
2024-04-28 07:38:22 +0000

    Deploying ReactJS Steps Message to GitHub

---
+++ README.md
@@ -0,0 +1,8 @@
+# ReactJS Steps Messages by Aldy Rizky Putra
+
+## Technology Used :
+
+- ReactJS by Vite
+- Use props and children
+- useState for rendering the views
+- EventHandler that apply in button or other Component
---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+const message = [
+  "Learn React! 📚",
+  "Develop Apps for Portfolio 👨",
+  "Get a Job as Frontend Developer 💻",
+];
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="main-title">ReactJS Steps Messages by Aldy Rizky Putra</h1>
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
+  const [steps, setSteps] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (steps > 1) setSteps((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (steps < message.length) setSteps((s) => s + 1);
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={steps >= 1 ? "active" : ""}>1</div>
+            <div className={steps >= 2 ? "active" : ""}>2</div>
+            <div className={steps >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {steps}: {message[steps - 1]}
+          </p>
+
+          <div className="buttons">
+            <Button
+              backgroundColor="#10439f"
+              textColor="#fff"
+              onClick={handlePrevious}
+            >
+              <span>👈</span>Previous
+            </Button>
+            <Button
+              backgroundColor="#10439f"
+              textColor="#fff"
+              onClick={handleNext}
+            >
+              Next<span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Button({ backgroundColor, textColor, onClick, children }) {
+  return (
+    <button
+      className="step-button"
+      style={{ backgroundColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
